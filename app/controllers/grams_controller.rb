8d9374e6eebff0edaf154b77b4e7a5331a630a29
jdoleiv <jdoleiv@gmail.com>
2017-01-30 10:38:09 +0800

    Gram detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -16,6 +16,13 @@ class GramsController < ApplicationController
   def index
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render text: 'Not Found :(', status: :not_found
+    end
+  end
+
   private
 
   def gram_params
